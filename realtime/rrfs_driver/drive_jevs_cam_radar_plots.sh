d2e4c3034bbd31f7474946c9d3c66bbca2b7f6df
marcel caron <marcel.caron@dlogin05.dogwood.wcoss2.ncep.noaa.gov>
2025-06-30 19:52:26 +0000

    Add 90-day plots

--- realtime/rrfs_driver/drive_jevs_cam_radar_plots.sh
+++ realtime/rrfs_driver/drive_jevs_cam_radar_plots.sh
@@ -20,6 +20,7 @@ for x in ${LINE_TYPES}; do
     echo "submitting jevs_cam_radar_plots.sh for ${x} linetype at ${vhr}Z"
     echo "vhr ${vhr}, LINE_TYPE ${x}, RRFSevs ${RRFSevs}"
     qsub -v vhr=$vhr,LINE_TYPE=$x ${RRFSevs}/dev/drivers/scripts/plots/cam/jevs_cam_radar_${x}_plots_last31days.sh
+    qsub -v vhr=$vhr,LINE_TYPE=$x ${RRFSevs}/dev/drivers/scripts/plots/cam/jevs_cam_radar_${x}_plots_last90days.sh
 done
 sleep 1
 
--- realtime/rrfs_driver/drive_jevs_cam_severe_plots.sh
+++ realtime/rrfs_driver/drive_jevs_cam_severe_plots.sh
@@ -19,6 +19,7 @@ LINE_TYPES="nbrcnt nbrctc"
 for x in ${LINE_TYPES}; do
     echo "submitting jevs_cam_severe_plots.sh for ${x} linetype at ${vhr}Z"
     qsub -v vhr=$vhr,LINE_TYPE=$x ${RRFSevs}/dev/drivers/scripts/plots/cam/jevs_cam_severe_${x}_plots_last31days.sh
+    qsub -v vhr=$vhr,LINE_TYPE=$x ${RRFSevs}/dev/drivers/scripts/plots/cam/jevs_cam_severe_${x}_plots_last90days.sh
     sleep 1
 done
 sleep 1
